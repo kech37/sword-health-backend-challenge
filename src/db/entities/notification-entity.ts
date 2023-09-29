e6721e7edeea7ccc28d08a2b259890952d0383f4
kech37 <carlos.p37@gmail.com>
2023-09-29 18:02:10 +0100

    Database schema, triggers and base data

---
+++ src/db/entities/notification-entity.ts
@@ -0,0 +1,43 @@
+/// <reference types='../../@types/global' />
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { NotificationType } from '../@types/notification-type';
+import { UserEntity } from './user-entity';
+
+@Entity('notification', { schema: 'public' })
+export class NotificationEntity extends BaseEntity {
+  @PrimaryColumn('uuid', {
+    name: 'id',
+    default: () => '(UUID())',
+  })
+  id!: UUID;
+
+  @Column('enum', {
+    name: 'status',
+    enum: NotificationType,
+    nullable: false,
+  })
+  type!: NotificationType;
+
+  @Column('uuid', {
+    name: 'to_user_id',
+    nullable: false,
+  })
+  toUserId!: UUID;
+
+  @Column('json', {
+    name: 'metadata',
+    nullable: false,
+  })
+  metadata!: object;
+
+  @Column('datetime', {
+    name: 'created_at',
+    default: () => '(NOW())',
+    nullable: false,
+  })
+  createdAt!: Date;
+
+  @ManyToOne(() => UserEntity, (entity) => entity.id)
+  @JoinColumn({ name: 'to_user_id' })
+  toUser!: UserEntity | null;
+}
---
+++ src/db/migrations/1696007803058-base-data.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class BaseData1696007803058 implements MigrationInterface {
+  name = 'BaseData1696007803058';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO \`public\`.\`role\` (\`name\`, \`can_own_create\`, \`can_own_read\`, \`can_own_update\`, \`can_own_delete\`, \`can_global_create\`, \`can_global_read\`, \`can_global_update\`, \`can_global_delete\`) VALUES ('MANAGER', '1', '1', '1', '1', '0', '1', '0', '1')`,
+    );
+    await queryRunner.query(
+      `INSERT INTO \`public\`.\`role\` (\`name\`, \`can_own_create\`, \`can_own_read\`, \`can_own_update\`, \`can_own_delete\`, \`can_global_create\`, \`can_global_read\`, \`can_global_update\`, \`can_global_delete\`) VALUES ('TECHNICIAN', '1', '1', '1', '0', '0', '0', '0', '0')`,
+    );
+    await queryRunner.query(
+      `INSERT INTO \`public\`.\`user\` (\`id\`, \`name\`, \`role_name\`) VALUES ('fa634d9f-7a47-4efb-b432-812d2b449dd5', 'Manager #1', 'MANAGER')`,
+    );
+    await queryRunner.query(
+      `INSERT INTO \`public\`.\`user\` (\`id\`, \`name\`, \`role_name\`) VALUES ('41f71b67-a18c-4374-87a4-b2ed044fdbde', 'Technician #1', 'TECHNICIAN')`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DELETE FROM \`public\`.\`user\` WHERE \`id\` = '41f71b67-a18c-4374-87a4-b2ed044fdbde'`);
+    await queryRunner.query(`DELETE FROM \`public\`.\`user\` WHERE \`id\` = 'fa634d9f-7a47-4efb-b432-812d2b449dd5'`);
+    await queryRunner.query(`DELETE FROM \`public\`.\`role\` WHERE \`name\` = 'TECHNICIAN'`);
+    await queryRunner.query(`DELETE FROM \`public\`.\`role\` WHERE \`name\` = 'MANAGER'`);
+  }
+}
