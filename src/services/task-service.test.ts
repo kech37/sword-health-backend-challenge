65ffffd6946206510d0f00c49e16a6322f9983f2
kech37 <carlos.p37@gmail.com>
2023-10-01 18:41:58 +0100

    Adds tests to task service update method

--- src/services/task-service.test.ts
+++ src/services/task-service.test.ts
@@ -4,6 +4,7 @@ import { HttpErrorCode } from '../@types/http-error-code';
 import { LogLevel } from '../@types/log-level';
 import { PaginatedResponse } from '../@types/paginated-response';
 import { Mock } from '../configs/mock';
+import { TaskStatus } from '../db/@types/task-status';
 import { TaskFacade } from '../db/facades/task-facade';
 import { UserFacade } from '../db/facades/user-facade';
 import { ApiBadRequestErrors, ApiForbiddenErrors, ApiNotFoundErrors } from '../errors/generic/api-errors';
@@ -22,7 +23,7 @@ function getPaginatedResponse<T>(result: T[]): PaginatedResponse<T> {
 }
 
 describe('Task service', () => {
-  const service = new SwordHealthBackendChallengeService(LogLevel.FATAL);
+  const service = new SwordHealthBackendChallengeService(LogLevel.SILENT);
 
   const sandbox = createSandbox();
 
@@ -158,7 +159,7 @@ describe('Task service', () => {
     });
   });
 
-  context.only('getById', () => {
+  context('getById', () => {
     it('should throw TASK_NOT_FOUND error', async () => {
       try {
         sandbox.stub(UserFacade.getInstance(service), 'getById').resolves(Mock.MANAGER_USER_1);
@@ -205,6 +206,63 @@ describe('Task service', () => {
       } catch (error) {
         expect(error).to.be.deep.equal(ErrorUtils.createApiError(Mock.REQUEST_ID, HttpErrorCode.HTTP_403_Forbidden, ApiForbiddenErrors.UnableToDetermineRole));
       }
+    });
+  });
+
+  context('update', () => {
+    it('should throw TASK_NOT_FOUND error', async () => {
+      try {
+        sandbox.stub(UserFacade.getInstance(service), 'getById').resolves(Mock.MANAGER_USER_1);
+        sandbox.stub(TaskFacade.getInstance(service), 'getById').resolves(undefined);
+
+        await TaskService.getInstance(service).update(Mock.REQUEST_ID, Mock.MANAGER_USER_1.id, 'INVALID TASK ID');
+      } catch (error) {
+        expect(error).to.be.deep.equal(ErrorUtils.createApiError(Mock.REQUEST_ID, HttpErrorCode.HTTP_404_NotFound, ApiNotFoundErrors.TaskNotFound));
+      }
+    });
+
+    it('should throw CANNONT_PERFORM_OPERATION error when task status is COMPLETED', async () => {
+      try {
+        sandbox.stub(UserFacade.getInstance(service), 'getById').resolves(Mock.MANAGER_USER_1);
+        sandbox.stub(TaskFacade.getInstance(service), 'getById').resolves({ ...Mock.TASK_1, status: TaskStatus.COMPLETED });
+
+        await TaskService.getInstance(service).update(Mock.REQUEST_ID, Mock.MANAGER_USER_1.id, Mock.TASK_1.id);
+      } catch (error) {
+        expect(error).to.be.deep.equal(ErrorUtils.createApiError(Mock.REQUEST_ID, HttpErrorCode.HTTP_403_Forbidden, ApiForbiddenErrors.CannotPerformOperation));
+      }
+    });
+
+    it('should throw CANNONT_PERFORM_OPERATION error when trying to update summary in a not status new task', async () => {
+      try {
+        sandbox.stub(UserFacade.getInstance(service), 'getById').resolves(Mock.MANAGER_USER_1);
+        sandbox.stub(TaskFacade.getInstance(service), 'getById').resolves({ ...Mock.TASK_1, status: TaskStatus.ON_GOING });
+
+        await TaskService.getInstance(service).update(Mock.REQUEST_ID, Mock.MANAGER_USER_1.id, Mock.TASK_1.id, undefined, 'SUMMARY UPDATE');
+      } catch (error) {
+        expect(error).to.be.deep.equal(ErrorUtils.createApiError(Mock.REQUEST_ID, HttpErrorCode.HTTP_403_Forbidden, ApiForbiddenErrors.CannotPerformOperation));
+      }
+    });
+
+    it('should throw CANNONT_PERFORM_OPERATION error when trying to update task from another user', async () => {
+      try {
+        sandbox.stub(UserFacade.getInstance(service), 'getById').resolves(Mock.TECHNICIAN_2);
+        sandbox.stub(TaskFacade.getInstance(service), 'getById').resolves(Mock.TASK_1);
+
+        await TaskService.getInstance(service).update(Mock.REQUEST_ID, Mock.TECHNICIAN_2.id, Mock.TASK_1.id);
+      } catch (error) {
+        expect(error).to.be.deep.equal(ErrorUtils.createApiError(Mock.REQUEST_ID, HttpErrorCode.HTTP_403_Forbidden, ApiForbiddenErrors.CannotPerformOperation));
+      }
+    });
+
+    it('should resolve on update', async () => {
+      sandbox.stub(UserFacade.getInstance(service), 'getById').resolves(Mock.TECHNICIAN_1);
+      sandbox.stub(TaskFacade.getInstance(service), 'getById').resolves(Mock.TASK_1);
+
+      const updatedTask = { ...Mock.TASK_1, summary: 'summary updated' };
+      sandbox.stub(TaskFacade.getInstance(service), 'update').resolves(updatedTask);
+
+      const result = await TaskService.getInstance(service).update(Mock.REQUEST_ID, Mock.TECHNICIAN_1.id, Mock.TASK_1.id);
+      expect(result).to.be.deep.equal(updatedTask);
     });
   });
 });
